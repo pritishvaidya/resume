808ad24fca78d21944f2a4d1621004456fb04e00
Pritish Vaidya <pritishvaidya94@gmail.com>
2020-12-25 17:54:58 +0530

    Update profile and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
-const { colors, fontFamily } = require('tailwindcss/defaultTheme');
+const { colors, flex, fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   theme: {
     colors: {
       primary: colors.gray,
-      secondary: colors.orange,
+      secondary: colors.purple,
       neutral: colors.gray,
       white: '#FFF',
     },
     extend: {
+      flex: {
+        ...flex,
+        0.3: '0.4 0.4 0%'
+      },
       fontFamily: {
         'sans': ['Open Sans', ...fontFamily.sans],
         'mono': ['Montserrat', ...fontFamily.mono]
